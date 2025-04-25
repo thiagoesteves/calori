f126329f34dcb80a1d16315b46d90873033a4144
Thiago Esteves <calori@gmail.com>
2025-04-25 17:37:36 -0300

    Testing Hot upgrade

--- lib/calori/worker.ex
+++ lib/calori/worker.ex
@@ -27,7 +27,7 @@ defmodule Calori.Worker do
   def handle_info(:schedule, state) do
     schedule_new_deployment(state.interval)
 
-    Logger.info("Running #{__MODULE__} Scheduler (/•ิ_•ิ)")
+    Logger.info("Running #{__MODULE__} Scheduler (/•ิ_•ิ) 🔥 (╯°□°)╯")
 
     {:noreply, state}
   end
