d949a41bd2f4483b6e4206e6db63ce6f6573c62f
Thiago Esteves <calori@gmail.com>
2025-05-31 16:48:48 -0300

    Updated for new deployex release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Calori.MixProject do
   def project do
     [
       app: :calori,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
